4ef329d62bd76f8c984ac6afa4530eb35bf08b31
David_Izosimov <David.Izosimov@gmail.com>
2019-08-29 16:20:09 +0200

    added index.html file content and index.features.js file, person and calculator tests still pass, but 1 failing for localhost test.

---
+++ features/index.feature.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const BrowserHelpers = require('e2e_training_wheels')
+const browser = new BrowserHelpers()
+
+describe('User can calculate BMI', () => {
+    before(async () => {
+        await browser.init()
+        await browser.visitPage('http://localhost:8080/')
+    });
+    
+beforeEach(async () => {
+    await browser.page.reload();
+    });
+
+after(async () => {
+    await browser.close();
+    });
+
+it('by inputing his weight and height', async () => {
+    await browser.fillIn("input[id='weight-in-kgs']", {with: "90" })
+    await browser.fillIn("input[id='height-in-cm']", { with: "186"})
+
+    await browser.clickOnButton("button")
+    let content = await browser.getContent("span[id='display_value']")
+    expect(content).to.eql('Your BMI is 26.01');
+    });
+});
