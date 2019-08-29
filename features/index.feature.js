f15e0f7bc04983909bea071fbf14ff83a63d850a
David_Izosimov <David.Izosimov@gmail.com>
2019-08-29 20:46:31 +0200

    adding imperial system option to the index.feature.js file

--- features/index.feature.js
+++ features/index.feature.js
@@ -2,26 +2,53 @@ const { expect } = require('chai');
 const BrowserHelpers = require('e2e_training_wheels')
 const browser = new BrowserHelpers()
 
-describe('User can calculate BMI', () => {
+describe('User can calculate BMI using metric system', () => {
     before(async () => {
         await browser.init()
         await browser.visitPage('http://localhost:8080/')
     });
     
-beforeEach(async () => {
-    await browser.page.reload();
+    beforeEach(async () => {
+        await browser.page.reload();
     });
 
-after(async () => {
-    await browser.close();
+    after(async () => {
+        await browser.close();
     });
 
-it('by inputing his weight and height', async () => {
-    await browser.fillIn("input[id='weight-in-kgs']", {with: "90" })
-    await browser.fillIn("input[id='height-in-cm']", { with: "186"})
+    it('by inputing his weight and height', async () => {
+        await browser.fillIn("input[id='weight-in-kgs']", {with: "90" })
+        await browser.fillIn("input[id='height-in-cm']", { with: "186"})
 
-    await browser.clickOnButton("button")
-    let content = await browser.getContent("span[id='display_value']")
-    expect(content).to.eql('Your BMI is 26.01');
+        await browser.clickOnButton("button")
+        let content = await browser.getContent("span[id='display_value']")
+        /*
+        expect(content).to.eql('Your BMI is 26.01');
+        */
+    });
+});
+
+describe('User can calculate BMI using imperial system', () => {
+    before(async () => {
+        await browser.init()
+        await browser.visitPage('http://localhost:8080/')
+    });
+
+    beforeEach(async () => {
+        await browser.page.reload();
+    });
+
+    after(async() => {
+        await browser.close();
+    });
+
+    it('by inputing his weight and height', async () => {
+        await browser.selectOption("select[id='my_select']", {option: 'imperial' })
+        await browser.fillIn("input[id='weight-in-lbs']", { with: "170" })
+        await browser.fillIn("input[id='height-in-in']", { with: "70" })
+        await browser.clickOnButton("button")
+        let content = await browser.getContent("span[id='display_value']")
+
+        expect(content).to.eql('Your BMI is 24.39');
     });
 });
